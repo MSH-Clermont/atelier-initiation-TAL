4dc66a73aef1ff56d570391cafc42f41abb2ad69
Aurelia <vasaura@gmail.com>
2023-03-16 10:58:23 +0100

    add input for users with a message to enter a path + test of argparse in annotateOneFile

--- annotateMultipleFiles.py
+++ annotateMultipleFiles.py
@@ -10,35 +10,30 @@ Created on : 07/03/2023 11:20
 import argparse
 from annotateText import write_xml
 
-# library for fetching the path names of files
-import glob
 # library to move through the folders
 import os
 
 
-"""
-parser = argparse.ArgumentParser()
 
-parser.add_argument("inputTextsFolder", help="dossier dans lequel se trouve tous les fichiers txt à lemmatiser. A ajouter en tant que nom de chemin")
+INPUTFOLDER = input("renseigner le chemin du dossier contenant des fichiers textes : \n")
 
-parser.add_argument("outputXmlFolder", help="dossier dans lequel seront sauvegarder les fichiers xml en sortie. A ajouter en tant que nom de chemin")
+# test if the path gave by users exists
+while os.path.exists(INPUTFOLDER) == False:
+    #if not, a message will be shown
+    INPUTFOLDER = input("chemin incorrect. renseigner le chemin du dossier contenant des fichiers textes : \n")
 
-args = parser.parse_args()
-
-inputTextFolder = args.inputTextsFolder
-outputXmlFolder = args.outputXmlFolder
-"""
-
-
-INPUTFOLDER = input("ajouter le chemin du dossier avec des textes")
-OUTPUFOLDER = input("ajouter le chemin des dossiers de sortie des xml")
-
-for fileText in os.listdir(INPUTFOLDER):
-
+# if the path is correct, the treatment of text files starts
+else:
     # for each text file
-    if fileText.endswith(".txt"):
-        # get the name without the extension .txt and add the xml extension instead
-        outputXmlFile = fileText[:-4] + ".xml"
+    for fileText in os.listdir(INPUTFOLDER):
+        # test if file exists
+        if fileText.endswith(".txt"):
+            # get the name without the extension .txt and add the xml extension instead
+            outputXmlFile = fileText[:-4] + ".xml"
+            # call write_xml method
+            write_xml(INPUTFOLDER+"/"+fileText, "outputXml/"+outputXmlFile)
+            # indicate for users the path of output xml
+            print("Le fichier xml se trouve dans outputXml/"+outputXmlFile)
 
-        write_xml(INPUTFOLDER+"/"+fileText, OUTPUFOLDER+"/"+outputXmlFile)
-        print(outputXmlFile)
+
+
