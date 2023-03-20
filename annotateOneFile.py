9b840ff0a1e6e5b4dac14830b731f2bf1290a52d
Kirill MASLINSKII <kimaslin>
2023-03-20 15:36:23 +0100

    remove unnecessary assignment

--- annotateOneFile.py
+++ annotateOneFile.py
@@ -15,10 +15,9 @@ parser.add_argument("inputTextFile", help="fichier texte en format .txt destiné
 args = parser.parse_args()
 # paramètre à rajouter par l'utilisateur avec le chemin vers le dossier contenant le fichier txt comme dans l'exemple
 #inputTextFile = "test/BUCA_Bastaire_Roman_Aventures_C95455.txt"
-inputTextFile = args.inputTextFile
 
 # sur le chemin récupéré, remplace l'extension .txt avec l'extension .xml (la sauvegarde du xml se fera dans le même dossier que le .txt
-outputXMLFile = inputTextFile[:-4] + ".xml"
+outputXMLFile = args.inputTextFile[:-4] + ".xml"
 
 # appelle la fonction write_xml pour générer un fichier xml sur un seul fichier txt.
 write_xml(inputTextFile, outputXMLFile)
